946c69084ae13c8aa08b571d925d037832e26998
RishaliNitJ <khushikumarilkr9097@gmail.com>
2024-11-05 23:56:57 +0530

    Added UpdateProduct function

--- shopaccount.cpp
+++ shopaccount.cpp
@@ -1,6 +1,6 @@
 #include "shopaccount.h"
 
-ShopAccount::ShopAccount(){
+ShopAccount::ShopAccount():m_category(""),m_name(""),m_price(-1),m_stock(-1){
     std::cout<<"ShopAccount Constructor invoked"<<std::endl;
 }
 
@@ -11,4 +11,22 @@ ShopAccount::~ShopAccount(){
 std::vector<std::string> ShopAccount::CheckAvailableProduct(){
     std::vector<std::string> availableProduct = {"Apple", "Mango"};
     return availableProduct;
+}
+void ShopAccount::UpdateProduct(double newPrice=-1,int newStock=-1,string newName="",string newCategory=""){
+    if(newPrice!=-1){
+        m_price=newPrice;
+        cout<<"price changes to"<<m_price<<endl;
+        }
+          if(newStock!=-1){
+        m_stock=newStock;
+        cout<<"stock changes to"<<m_stock<<endl;
+        }
+          if(!newName.empty()){
+        m_name=newName;
+        cout<<"name changes to"<<m_name<<endl;
+        }
+         if(!newCategory.empty()){
+        m_category=newCategory;
+        cout<<"name changes to"<<m_category<<endl;
+        }
 }
--- shopaccount.h
+++ shopaccount.h
@@ -1,6 +1,7 @@
 #include <vector>
 #include <string>
 #include <iostream>
+using namespace std;
 
 class ShopAccount{
     public:
@@ -9,4 +10,11 @@ class ShopAccount{
 
     public:
     std::vector<std::string> CheckAvailableProduct();
+    void UpdateProduct(double newPrice=-1,int newStock=-1,string newName="",string newCategory="");
+
+    private:
+    string m_name;
+    string m_category;
+    double m_price;
+    int m_stock;
 };
